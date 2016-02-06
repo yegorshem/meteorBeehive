5d8d499c6ac7a1cba332527b55ff931f7d63c5e7
yegorshem <yshemereko@mail.greenriver.edu>
2016-02-06 15:00:49 -0800

    beehive form - fully functional

--- beehive/beehive.js
+++ beehive/beehive.js
@@ -1,23 +1,57 @@
+BeeData = new Mongo.Collection("beeData");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    //current website subscribes to the beeData collection of the Mongo database
+    Meteor.subscribe("beeData");
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    //a function that orders all the data submitted
+    Template.dataTable.helpers({
+            "beeData": function () {
+                return BeeData.find({}, {sort: {createdOn: -1}}) || {};
+            }
+        }
+    );
+
+    //prevents form from redirecting and inserts data into BeeData database
+    Template.form.events({
+        "submit form": function (event) {
+            event.preventDefault();
+
+            var hiveID = ($(event.target).find('input[name=hiveID]')).val();
+
+            var collectionDate = ($(event.target).find('input[name=collectionDate]')).val();
+
+            var samplePeriod = ($(event.target).find('input[name=samplePeriod]')).val();
+
+            var miteCount = ($(event.target).find('input[name=miteCount]')).val();
+
+            //if not empty then insert
+            if (hiveID.length > 0) {
+
+                BeeData.insert(
+                    {
+                        hiveID: hiveID,
+                        collectionDate: collectionDate,
+                        samplePeriod: samplePeriod,
+                        miteCount: miteCount,
+                        createdOn: Date.now()
+                    }
+                );
+            }
+            else {
+                alert("All fields are required.")
+            }
+        }
+
+    });
+
+
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+
+    Meteor.publish("beeData", function () {
+        return BeeData.find();
+    });
 }
